cf10aef69f8b25aec3c9310da5530fdf44299549
Lorena Spiller <lorenaspiller@gmail.com>
2021-03-18 16:13:45 +0100

    aggiunto campo input e reso funzionale

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ var toDoList = [
 var list = $('.todo-list li ');
 
 // Attraverso l'array
-//
+
 for (var i = 0; i < toDoList.length; i++) {
   // Clono il mio li nel template
   var template = $('.template ul li').clone();
@@ -27,6 +27,42 @@ for (var i = 0; i < toDoList.length; i++) {
   // Aggiungo il li del template alla .todo-list
   list.append(template);
 }
+
+
+// Rendo il campo imput funzionale all'evento della pressione del tasto invio
+
+var add = $('#todo-list-add');
+
+add.keydown(function(e){
+
+  if (e.which == 13) {
+    // prendo il valore del testo immesso nel campo input
+    var text = $(this).val();
+    // console.log(text);
+
+
+    //controllo che il campo input non sia vuoto, così da non aggiungere elementi vuoti alla lista
+
+    if (text != "") {
+      // Clono il mio li nel template
+      var template = $('.template ul li').clone();
+
+      // Aggiungo gli elementi dell'array della to do list al li del template
+      template.prepend(text);
+
+      // Aggiungo il li del template alla .todo-list
+      list.append(template);
+
+      // Svuoto il campo input una volta inserito l'elemento digitato
+      $(this).val("");
+    }
+
+  }
+
+
+});
+
+
 
 
 
